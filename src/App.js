b7a19e759c2fc723dde5a8f3a020b98a0029edd4
JoshuaOrlandoR <joshuaorlando.r@gmail.com>
2023-03-08 17:35:34 -0500

    Finished first attempt at Footer component, continue to build other components and the rest of code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
-import Header from './components/Header'
-import About from './components/About'
-import Projects from './components/Projects'
-import Footer from './components/Footer'
+import Header from './components/Header';
+import About from './components/About';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
+
 
 
 function App() {
   return (
-  <main>
+  <div>
     <Header> </Header>
     <About> </About>
     <Projects> </Projects>
     <Footer> </Footer>
-  </main>
+  </div>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,21 @@
-export default function Header() {}
+import React, { useState } from 'react';
+import About from "./About";
+import Navigation from "./Navigation";
+import Projects from "./Projects";
+import Footer from "./Footer";
+// This is just placeholder garbage for now
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#About">About</a></li>
+          <li><a href="#Projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
