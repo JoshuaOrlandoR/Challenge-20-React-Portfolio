0e5b2e481db7713cb958da71247a56de17fae4b2
JoshuaOrlandoR <joshuaorlando.r@gmail.com>
2023-03-10 17:45:14 -0500

    Finished header component in showcase, need to work on project section next

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,32 +1,44 @@
 import React from 'react';
-import './css/Navigation.css';
+import ButtonComponent from './ButtonComponent';
 
 function Navigation(props) {
   return (
-    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark' style={{position: 'sticky', top: 0}}>
       <div className='container-fluid justify-content-between'>
-        <div className='navbar-brand'>Joshua Orlando</div>
+        <div className='navbar-brand text-white'>Joshua Orlando</div>
 
         <ul className='navbar-nav'>
           <li className='nav-item'>
-            <button className={`nav-link ${props.activeSection === 'showcase' ? 'active' : ''}`} onClick={() => props.setActiveSection('showcase')}>
-              Home
-            </button>
+            <ButtonComponent 
+              text='Home' 
+              onClick={() => props.setActiveSection('showcase')}
+              activeSection={props.activeSection}
+              sectionName="showcase"
+            />
           </li>
           <li className='nav-item'>
-            <button className={`nav-link ${props.activeSection === 'aboutme' ? 'active' : ''}`} onClick={() => props.setActiveSection('aboutme')}>
-              About Me
-            </button>
+            <ButtonComponent 
+              text='About Me' 
+              onClick={() => props.setActiveSection('aboutme')}
+              activeSection={props.activeSection}
+              sectionName="aboutme"
+            />
           </li>
           <li className='nav-item'>
-            <button className={`nav-link ${props.activeSection === 'contact' ? 'active' : ''}`} onClick={() => props.setActiveSection('contact')}>
-              Contact
-            </button>
+            <ButtonComponent 
+              text='Contact' 
+              onClick={() => props.setActiveSection('contact')}
+              activeSection={props.activeSection}
+              sectionName="contact"
+            />
           </li>
           <li className='nav-item'>
-            <button className={`nav-link ${props.activeSection === 'resume' ? 'active' : ''}`} onClick={() => props.setActiveSection('resume')}>
-              Resume
-            </button>
+            <ButtonComponent 
+              text='Resume' 
+              onClick={() => props.setActiveSection('resume')}
+              activeSection={props.activeSection}
+              sectionName="resume"
+            />
           </li>
         </ul>
       </div>
